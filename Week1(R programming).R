ca6ec8dfedcc9b3e871ec319367bd381aa47cf23
Khashhatami <wecan9732@gmail.com>
2022-01-24 05:46:43 -0500

    First week

---
+++ Week1(R programming).R
@@ -0,0 +1,8 @@
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
+
+#testing the function
+myMean(assignment2)
+
+#testing the function against the R's mean function
+mean(assignment2)
