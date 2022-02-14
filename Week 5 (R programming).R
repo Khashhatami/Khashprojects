911be3ce1e7ba7ba3ca4282323cd28071bae8c34
Khashhatami <wecan9732@gmail.com>
2022-02-14 01:40:50 -0500

    DOing some math in R

---
+++ Week 5 (R programming).R
@@ -0,0 +1,16 @@
+
+
+# martices
+A <- matrix(1:100, nrow=10) 
+B <- matrix(1:1000, nrow=10)
+
+# inverses of matrices
+A_inv <- solve(A)
+
+B_inv <- solve(B)
+
+# determinant of matrices
+det(A)
+
+det(B)
+
