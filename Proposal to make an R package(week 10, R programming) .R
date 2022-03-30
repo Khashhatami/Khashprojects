2cfa04c447de18d81376ebe3e367ff86186e865e
Khashhatami <wecan9732@gmail.com>
2022-03-30 18:08:26 -0400

    Here i lay out my proposal for creating an R package which automates basic semantic analytics functions for creating a word cloud as well as providing a ratio positive and negative words from txt file

---
+++ Proposal to make an R package(week 10, R programming) .R
@@ -0,0 +1,17 @@
+Package: autoTextAnalysis
+Type: Package
+Title: Cleans up text and perfrom basic text analysis 
+Author: "Khashayar Hatami <https://github.com/Khashhatami/Khashprojects> [aut, cre]"
+Description: This package accepts txt files, and after cleaning them up 
+(removing stop words, punctucations, etc), conducts basic semantic analytics on them. 
+These analytics functions include : 1. creating a word cloud to highlight the most important words
+2.Utilizing lexicon_bing dictionary to give a ration of negative and positive words to the user as well 
+as a char that shows that ratio 
+Data from: https://www.artofmanliness.com/character/knowledge-of-men/the-35-greatest-speeches-in-history/#Theodore_Roosevelt_duties_Of_American_Citizenship
+  Depends:
+  R (>= 3.1.2),
+scales
+License: CC0
+Encoding: UTF-8
+LazyData: true
+RoxygenNote: 7.1.0.9000
