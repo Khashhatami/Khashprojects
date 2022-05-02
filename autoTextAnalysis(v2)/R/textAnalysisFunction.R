2d811702f568389d0b0fed8fb93b160168320cd8
Khashhatami <89555366+Khashhatami@users.noreply.github.com>
2022-05-02 02:51:08 -0400

    Add files via upload

---
+++ autoTextAnalysis(v2)/R/textAnalysisFunction.R
@@ -0,0 +1,112 @@
+
+#' autoTextAnalysis
+#'
+#'This package accepts txt files, and after cleaning them up 
+#'(removing stop words, punctucations, etc), conducts basic semantic analytics on them. 
+#'These analytics functions include : 1. creating a word cloud to highlight the most important words
+#'2.Utilizing lexicon_bing dictionary to give a ration of negative and positive words to the user as well 
+#'as a char that shows that ratio 
+#'
+#' @param infile Path to the input file
+#' @return Word Clouds and negative/positive word ratio
+#' @export
+textAnalysisFunction<- function(input){
+  
+ 
+  
+  #use scan to read the file
+  inputText <- scan(input, character(0),sep = ".")
+  
+  words.vec <- VectorSource(inputText)
+  words.corpus <- Corpus(words.vec)
+  words.corpus <- tm_map(words.corpus, content_transformer(tolower))
+  words.corpus <- tm_map(words.corpus, removePunctuation)
+  words.corpus <- tm_map(words.corpus, removeNumbers)
+  words.corpus <- tm_map(words.corpus, removeWords, stopwords("english"))
+  tdm <- TermDocumentMatrix(words.corpus)
+  m <- as.matrix(tdm)
+  wordCounts <- rowSums(m)
+  wordCounts <- sort(wordCounts, decreasing=TRUE)
+  head(wordCounts)
+  
+  cloudFrame <- data.frame(word = names(wordCounts), freq=wordCounts)
+  couldbw<-wordcloud(cloudFrame$word, cloudFrame$freq)
+  couldColor<-wordcloud(names(wordCounts), wordCounts, min.freq=2, max.words=50, rot.per=0.35, 
+            colors=brewer.pal(8, "Dark2"))
+  
+  
+  
+  get_sentiments("bing")
+  #then in console, select 1
+  
+  pos <- "C:\\Users\\wecan\\OneDrive\\Documents\\bing\\positive-words.txt"
+  neg <- "C:\\Users\\wecan\\OneDrive\\Documents\\bing\\negative-words.txt"
+  
+  #now we have read both the positive and the negative words, we should put them in vectors
+  p <- scan(pos, character(0),sep = "\n")
+  n <- scan(neg, character(0),sep = "\n")
+  
+  #one we print out the head of both vectors, we notice that roughly the first 30 elements of our 
+  #vectors are infact extra information which are not useful for our code, we are just going to take them
+  #out
+  head(p, 50)
+  head(n, 50)
+  
+  p <- p[-1:-34]
+  n <- n[-1:-34]
+  head(p, 10)
+  head(n, 10)
+  
+  #totalWords is the sum of the all the terms used in inputText spa.txt in the order of their frequency
+  totalWords <- sum(wordCounts)
+  
+  #words is the actual terms in the speech which are aquired by using the names function
+  words <- names(wordCounts)
+  
+  #now we use the match function to determine how many of the words used in inputText.txt are are found
+  #within the positive words dictionary. the ones that there are no match for (nuetral words for instance)
+  #are simply designated to be 0
+  
+  matched <- match(words, p, nomatch = 0) 
+  
+  #Now we have a vector of all positive words from our dictionary that are matched with positive words
+  #with our inputText.txt speech
+  
+  
+  #mCounts finds which words in matched vector are not 0 (in other words which words were matched from 
+  #our dictionary to our speech)
+  #mCounts vector therefore includes only words that were matched
+  
+  mCounts <- wordCounts[which(matched != 0)]
+  
+  #now let's see how many positive words are there in total
+  mWords <- names(mCounts)
+  nPos <- sum(mCounts)
+
+  #[1] 17
+  
+  # we will do the same for our negative words:
+  matched <- match(words, n, nomatch = 0)
+  nCounts <- wordCounts[which(matched != 0)]
+  nNeg <- sum(nCounts)
+  nWords <- names(nCounts)
+  
+  #here we modified the totalWords to be the length of all words in the speech
+  #"words" was the name of all the terms in wordCounts.
+  totalWords <- length(words)
+  
+  #Now let's compare the ratio of the all positive words used in the speech against all the words in
+  #the pseech as well as the ratio of all negative words used in the speech against all the words in 
+  #the speech
+  ratioPos <- nPos/totalWords
+  ratioPos<- ratioPos*100
+  ratioNeg <- nNeg/totalWords
+  ratioNeg<- ratioNeg*100
+  
+  mylist<- list("Percent of Positive words"=ratioPos,"Percent of Negative words"=ratioNeg,"Black and White Word-Cloud"=couldbw,"Colorful Word-Cloud"=couldColor)
+  return(mylist)
+}
+
+devtools::document()
+
+
